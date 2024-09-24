1ff1b241c31bea6a9d58d9bb0951454a6c6e1981
Sachin Das <sachin.oasys@gmail.com>
2024-09-24 17:49:14 +0530

    Table Data Change

--- src/components/users/UserData.tsx
+++ src/components/users/UserData.tsx
@@ -3,12 +3,12 @@ import { User } from "./User";
 export const UserData: User[] = [
   {
     id: 1,
-    name: "John",
-    email: "john@example.com",
+    name: "Sachin Das",
+    email: "sachin.das@gmail.com",
   },
   {
     id: 2,
-    name: "John",
-    email: "john@example.com",
+    name: "Oasys Sachin",
+    email: "sachin.das@gmail.com",
   },
 ];
